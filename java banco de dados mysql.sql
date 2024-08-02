a48753e9d71515ba0d89a3ee1602598852fec837
17rzpio <74218387+17rzpio@users.noreply.github.com>
2024-08-02 16:16:44 -0300

    Add files via upload
    curso de java basico e avançado da escola virtual bradesco

---
+++ java banco de dados mysql.sql
@@ -0,0 +1,33 @@
+/*use cadastro;
+create table books
+(
+id int unsigned not null auto_increment,
+title varchar(255) not null,
+author varchar(255) not null,
+price decimal(10,2) not null,
+
+primary key (id)
+);
+insert into books (title,author,price)
+values ("d","tori",8.8);*/
+/*
+select * from books;
+select * from books where id=2;
+select * from books where price <70 and price >5;
+select title;*/
+use cadastro;
+/*create table Carros
+(id int not null  auto_increment,
+nome varchar(50) not null,
+modelo varchar(50),
+motor varchar(50) not null,
+fabricante varchar(50) not null,
+anoFabricacao int not null,
+anoModelo int not null,
+cor varchar(50) not null,
+primary key (id)
+);*/
+/*insert into Carros (nome,modelo,motor,fabricante,anoFabricacao,anoModelo,cor)
+values("xsara","picasso","oito cilindros","citroen",2009,2002,"preto"),
+("c5","pgls","dezesseis cilindros","citroen",2004,2000,"preto");*/
+select * from Carros;
